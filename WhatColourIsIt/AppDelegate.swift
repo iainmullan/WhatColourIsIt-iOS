3485045e2b99c5ccabdc3fabaf1d741fb6e44082
Iain Mullan <iain.mullan@gmail.com>
2014-12-17 14:25:58 +0000

    basically works

--- WhatColourIsIt/AppDelegate.swift
+++ WhatColourIsIt/AppDelegate.swift
@@ -10,37 +10,46 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        application.idleTimerDisabled = true // Turn off sleep mode
+        
         return true
     }
-
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        
+        application.idleTimerDisabled = false
     }
-
+    
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        application.idleTimerDisabled = false
     }
-
+    
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        application.idleTimerDisabled = true
     }
-
+    
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        application.idleTimerDisabled = true
     }
-
+    
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        application.idleTimerDisabled = false
     }
-
-
+    
+    
 }
 
--- WhatColourIsIt/ViewController.swift
+++ WhatColourIsIt/ViewController.swift
@@ -8,18 +8,96 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(netHex:Int) {
+        self.init(red:(netHex >> 16) & 0xff, green:(netHex >> 8) & 0xff, blue:netHex & 0xff)
+    }
+}
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    var displayFormatter: NSDateFormatter;
+    var colourFormatter: NSDateFormatter;
+    var timer: NSTimer;
+    
+    required init(coder aDecoder: NSCoder) {
+        
+        self.displayFormatter = NSDateFormatter()
+        self.displayFormatter.dateFormat = "HH:mm:ss"
+        
+        self.colourFormatter = NSDateFormatter()
+        self.colourFormatter.dateFormat = "HHmmss"
+        
+        self.timer = NSTimer()
+        
+        super.init(coder: aDecoder)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        startTimer()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func startTimer() {
+        
+        updateTime()
+        
+        let aSelector : Selector = "updateTime"
+        
+        self.timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: aSelector, userInfo: nil, repeats: true)
+        
+    }
+    
+    func stopTimer()
+    {
+        self.timer.invalidate()
+    }
+    
+    func updateTime() {
+        
+        var now = NSDate()
+        
+        //code to set date style.....
+        var displayTime: NSString = self.displayFormatter.stringFromDate(now)
+        self.timeLabel.text = displayTime
+        
+        var colorTime: NSString = self.colourFormatter.stringFromDate(now)
+        
+        let scanner = NSScanner(string: "0x"+colorTime)
+        
+        var result : UInt32 = 0
+        
+        scanner.scanHexInt(&result)
+        
+        var resultInt : Int = Int(result)
+        
+        self.view.backgroundColor = UIColor(red:(resultInt >> 16) & 0xff, green:(resultInt >> 8) & 0xff, blue:resultInt & 0xff)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        startTimer()
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        stopTimer()
+    }
+    
 }
-
